13588a693a26e207db5b4c24cb07b6953f54d93a
Mahumane Junior <131586974+alexandre-mahumane@users.noreply.github.com>
2025-10-16 06:48:32 +0200

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,30 @@
-import type { Request, Response } from "express";
-import { userService } from "./user.service.ts";
-
+import type { Request, Response } from 'express'
+import { userService } from './user.service.ts'
+//hello
 async function create(req: Request, res: Response) {
-  const created = await userService.create(req.body);
-  res.status(201).json(created);
+  const created = await userService.create(req.body)
+  res.status(201).json(created)
 }
 
 async function list(_req: Request, res: Response) {
-  res.json(await userService.list());
+  res.json(await userService.list())
 }
 
 async function get(req: Request<{ id: string }>, res: Response) {
-  const item = await userService.get(req.params.id);
-  if (!item) return res.status(404).end();
-  res.json(item);
+  const item = await userService.get(req.params.id)
+  if (!item) return res.status(404).end()
+  res.json(item)
 }
 
 async function update(req: Request<{ id: string }>, res: Response) {
-  const updated = await userService.update(req.params.id, req.body);
-  if (!updated) return res.status(404).end();
-  res.json(updated);
+  const updated = await userService.update(req.params.id, req.body)
+  if (!updated) return res.status(404).end()
+  res.json(updated)
 }
 
 async function remove(req: Request<{ id: string }>, res: Response) {
-  await userService.remove(req.params.id);
-  res.status(204).end();
+  await userService.remove(req.params.id)
+  res.status(204).end()
 }
 
 export const userController = {
@@ -33,6 +33,4 @@ export const userController = {
   get,
   update,
   remove,
-};
-
-
+}
